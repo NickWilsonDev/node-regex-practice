ea35871029f51e0ddc06be23fb7cc872e6b1de4a
Nick Wilson <logikfx@gmail.com>
2018-03-22 00:20:32 -0400

    added a regex that is able find episode title, number and air date of star trek episodes from the wiki page html

--- example.js
+++ example.js
@@ -16,15 +16,17 @@ assert(regex.test('F'), "1 letter symbol");
 
 
 // credit card numbers
-regex = /(^[0-9]{16}$)|(^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4})/
+regex = /(^[0-9]{16}$)|(^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4})/;
 assert(regex.test('4000000000000000'), '16 digit number no dashes');
 
 assert(regex.test('4000-0000-0000-0000'), '16 digit number with dashes');
 
 
 //href links not quite done
-regex = /^http[s]?:\/\/([a-zA-Z].)*(google|Google).com([\/][a-zA-Z0-9\-]+)*$/
+regex = /^http[s]?:\/\/([a-zA-Z].)*(google|Google).com([\/][a-zA-Z0-9\-]+)*$/;
 //regex = /^http[s]?:\/\/([a-zA-Z].)?[a-zA-Z0-9]+[a-zA-Z0-9\-]*(\/[a-zA-Z0-9\-])*.[a-zA-Z]{2,10}$/
 
 
+// star trek episode scraping from wiki page
+regex = /(\<th scope=\"row\" id=\"ep[0-9]+\" style=\"text-align:center\"\>([0-9]+))|(\<td class=\"summary\" style=\"text-align:left\"\>\"\<a href=\"\/wiki\/[a-zA-Z0-9_\(\):]+\" title=\"[a-zA-Z0-9 \(\)_:]+\"\>([a-zA-Z0-9 ]+)\<\/a\>\"\<\/td\>)|(\<td\>(([a-zA-Z]+)\&nbsp;([0-9]{1,2}),\&nbsp;([0-9]{4}))\<span style=\"display:none\"\>&nbsp;\(\<span class=\"bday dtstart published updated\"\>[0-9\-]+\<\/span\>\)\<\/span\>\<\/td\>)/;
 
